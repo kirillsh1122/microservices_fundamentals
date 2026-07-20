db03967d9b30afc0122a4c3dae2305dff8422faf
Kiryl Shapialevich <kirillshepelevich@gmail.com>
2026-07-20 21:04:14 +0200

    Added spring-retry for resource-service

--- maven-project/resource-service/src/main/java/com/microservice/architecture/overview/resource_service/service/ResourceMessagingServiceImpl.java
+++ maven-project/resource-service/src/main/java/com/microservice/architecture/overview/resource_service/service/ResourceMessagingServiceImpl.java
@@ -2,8 +2,13 @@ package com.microservice.architecture.overview.resource_service.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.KafkaException;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ExecutionException;
 
 @Slf4j
 @Service
@@ -12,9 +17,22 @@ public class ResourceMessagingServiceImpl implements ResourceMessagingService {
     @Autowired
     private KafkaTemplate<String, Long> kafkaTemplate;
 
+    @Retryable(
+            retryFor = KafkaException.class,
+            maxAttempts = 3,
+            backoff = @Backoff(delay = 1000, multiplier = 2)
+    )
     @Override
     public void sendResourceCreatedMessage(Long resourceId) {
-        kafkaTemplate.send("resource-topic", resourceId);
-        log.info("Sent resource created message for resource ID: {}", resourceId);
+        try {
+            kafkaTemplate.send("resource-topic", resourceId).get();
+            log.info("Sent resource created message for resource ID: {}", resourceId);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new KafkaException("Interrupted while sending message", e);
+
+        } catch (ExecutionException e) {
+            throw new KafkaException("Failed to send Kafka message", e);
+        }
     }
 }
