78fda4e7aee615f39831d8aa8a8225b4e4350e57
Kiryl Shapialevich <kirillshepelevich@gmail.com>
2026-08-02 00:45:59 +0200

    Added health checks for song-service

---
+++ maven-project/song-service/src/main/java/com/microservice/architecture/overview/song_service/model/ContainerEnvironment.java
@@ -0,0 +1,50 @@
+package com.microservice.architecture.overview.song_service.model;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Slf4j
+@Getter
+@Setter
+@Component
+public class ContainerEnvironment {
+    private String appVersion;
+    private String appDate;
+    private String containerHostName;
+
+    @PostConstruct
+    private void initialize() {
+        try {
+            this.setContainerHostName(
+                    InetAddress.getLocalHost().getHostAddress() + "/" + InetAddress.getLocalHost().getHostName());
+        } catch (UnknownHostException e) {
+            this.setContainerHostName("unknown");
+        }
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            ClassPathResource resource = new ClassPathResource("version.json");
+
+            try (InputStream inputStream = resource.getInputStream()) {
+                Version version = objectMapper.readValue(inputStream, Version.class);
+                this.setAppVersion(version.getVersion());
+                this.setAppDate(version.getDate());
+            }
+        } catch (IOException e) {
+            log.error("Error parsing file {}", e.getMessage());
+            this.setAppVersion("unknown");
+            this.setAppDate("unknown");
+        }
+    }
+
+}
